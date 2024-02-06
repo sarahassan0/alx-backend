39aa7d29e67b3a038304bc62930c6b4faab170b2
sarahassan0 <sarraree5@gmail.com>
2024-02-06 22:57:06 +0200

    .

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -33,7 +33,7 @@ def get_locale() -> str:
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index() -> str:
     """
         App entry point

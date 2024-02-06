90002fe613f31d5bc8bb8661ea627845c4570ca0
sarahassan0 <sarraree5@gmail.com>
2024-02-06 22:15:11 +0200

    edit: edit rendered template

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -38,7 +38,7 @@ def index() -> str:
     """
         App entry point
     """
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
